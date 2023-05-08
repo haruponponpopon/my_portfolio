ec2127124cd4cb3b4f3f80dd8a648159055667a6
iba pc <iba_lab_pc2023@ibanoMacBook-Pro.local>
2023-05-08 23:17:07 +0900

    commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -53,10 +53,14 @@ export default function Skills() {
                 {/* <SkillCard name="react" experience="1 years" img={react} /> */}
                 {/* <SkillCard name="cisco" experience="1 years" img={cisco} /> */}
                 <SkillCard name="docker" experience="1 years" img={docker} />
-                <SkillCard name="git" experience="2 years" img={git} />
+                <SkillCard name="git" experience="3 years" img={git} />
+                <SkillCard name="c#" experience="2 years" img={"https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png"} />
+                <SkillCard name="Unity" experience="2 years" img={"https://s26.q4cdn.com/977690160/files/design/U_Logo_White_RGB.png"} />
+                <SkillCard name="SQL" experience="1 years" img={"https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png"} />
+                <SkillCard name="swift" experience="1 years" img={"https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/23-swift-512.png"} />
                 <SkillCard name="mac" experience="4 years" img={"https://e7.pngegg.com/pngimages/903/667/png-clipart-apple-logo-macbook-pro-iphone-apple-angle-heart.png"} />
                 <SkillCard name="c++" experience="3 years" img={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/640px-ISO_C%2B%2B_Logo.svg.png"} />
-                <SkillCard name="PyTorch" experience="3 years" img={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/PyTorch_logo_icon.svg/640px-PyTorch_logo_icon.svg.png"} />
+                <SkillCard name="PyTorch" experience="1 years" img={"https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/PyTorch_logo_icon.svg/640px-PyTorch_logo_icon.svg.png"} />
                 {/* <SkillCard name="express" experience="1 years" img={express} /> */}
                 {/* <SkillCard name="gitlab" experience="1 years" img={gitlab} /> */}
                 </Slider>
